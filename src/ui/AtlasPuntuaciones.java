4bf05f8f25d7b45e24d01741b8b99538d9dd260e
Juan Nieto05 <juanf.nieto16@gmail.com>
2025-03-07 22:54:47 -0500

    Seguimiento 2

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -1,10 +1,3 @@
-// Análisis (completar)
-// Descripción del programa:
-// Entradas:
-// Salidas:
-// Ejemplo:
-
-
 package ui; // No olvidar especificar el paquete / carpeta de nuestro programa
 
 import java.util.Scanner; // Para poder usar Scanner, debemos importarlo
@@ -15,29 +8,32 @@ public class AtlasPuntuaciones {
 	private Scanner escaner;
 
 	// Declaración de nuestras constantes a utilizar
-
+	final static int MAXIMOJUGADORES = 10;
+	final static int MAXIMORONDAS = 5;
 
 	private AtlasPuntuaciones() {
 		escaner = new Scanner(System.in);
 	}
 	
 	
-	public void run()
-	{
-		// Declaracion de todas las variables a usar en el programa
+	public void run(){
+		
+		int numJugadores = 0;
+		System.out.println("Ingresar cantidad de jugadores:");
+		numJugadores = escaner.nextInt();
 
+		int numRondas = 0;
+		System.out.println("Ingresar la cantidad de rondas:");
+		numRondas = escaner.nextInt();
+			
 
-		// Notificacion al usuario de la solicitud de dato
+		//Arreglos
+		int[] jugadores = new int[numJugadores];
+		double[] promedioJugadores = new double[numJugadores];
 
-		// Capturamos el dato con nuestro Scanner (que se llama, escaner)
-
-
-
-		// Realizamos los cálculos delegandolos a los métodos
-
-
-		// Notificación al usuario de los resultados
-
+		calcularSumaTotal(numJugadores, numRondas, jugadores);
+		calcularPromedio(numJugadores, numRondas, jugadores, promedioJugadores);
+		encontrarMayor(numJugadores, jugadores);
 
 	}
 
@@ -46,31 +42,77 @@ public class AtlasPuntuaciones {
 		mainApp.run();
 	}
 
-	/**
-	 * Descripción: El método calcularSumaTotal permite ...
-	 * @param int[] numeros
-	 * @return 
-	 */
-    public int calcularSumaTotal(int[] numeros) {
-        // Completar
-    }
+	
 
 	/**
-	 * Descripción: El método calcularPromedio permite ...
-	 * @param int[] numeros
-	 * @return 
+	 * Descripción: El método calcularSumaTotal permite sumar el total de puntajes de cada jugador.
+	 * @param int cantidadJugadores La cantidad total de jugadores en la partida.
+	 * @param int cantidadRondas La cantidad de rondas que se jugaran durante la partida.
+	 * @param [] jugadores Un arreglo donde se almacena el total de puntajes de cada jugador.
+	 * @return Retorna la informacion de cada jugador.
 	 */
-    public double calcularPromedio(int[] numeros) {
-        // Completar
-    }
+	public void calcularSumaTotal(int cantidadJugadores, int cantidadRondas, int[] jugadores) {
+		for (int i = 0; i < cantidadJugadores; i++) {
+			jugadores[i] = 0; 
+	
+			for (int j = 1; j <= cantidadRondas; j++) {
+				int puntaje = 0;
+				boolean flag = true;
+	
+				while (flag) {
+					System.out.println("Jugador " + (i + 1) + " - Puntaje de la ronda " + j + ":");
+					puntaje = escaner.nextInt();
+	
+					if (puntaje >= 0 && puntaje <= 100) {
+						flag = false; 
+					} else {
+						System.out.println("Puntaje inválido. Ingrese un valor entre 0 y 100.");
+					}
+				}
+	
+				jugadores[i] += puntaje; 
+			}
+		}
+	}
+	
+	   
 
 	/**
-	 * Descripción: El método encontrarMayor permite ...
-	 * @param double[] numeros
-	 * @return 
+	 * Descripción: El método calcularPromedio permite sacar el promedio de puntajes de cada jugador durante la partida.
+	 * @param int cantidadJugadores La cantidad total de jugadores en la partida.
+	 * @param int cantidadRondas La cantidad de rondas que se jugaran durante la partida.
+	 * @param [] jugadores Un arreglo donde se almacena el total de puntajes de cada jugador.
+	 * @param [] promedioJugadores Es el arreglo donde se almacena los promedios de cada jugador.
+	 * @return El promedio del puntaje de los jugadores a lo largo de la partida.
 	 */
-    public double encontrarMayor(double[] numeros) {
-        // Completar
+	public void calcularPromedio(int cantidadJugadores, int cantidadRondas, int[] jugadores, double[] promedioJugadores) {
+		System.out.println("Promedio jugadores");
+		for (int i = 0; i < cantidadJugadores; i++) {
+			promedioJugadores[i] = (double) jugadores[i] / cantidadRondas; // Calcula el promedio
+			System.out.println("Jugador " + (i + 1) + " - Promedio: " + promedioJugadores[i]);
+		}
+	}
+	
+
+	/**
+	 * Descripción: El método encontrarMayor permite encontrar el jugador con la cantidad maxima de puntajes.
+	 * @param int cantidadJugadores La cantidad total de jugadores en la partida.
+	 * @param [] jugadores Un arreglo donde se almacena el total de puntajes de cada jugador.
+	 * @return El jugador con mayor puntaje.
+	 */
+  	
+	 public void encontrarMayor(int cantidadJugadores, int[] jugadores) {
+		int maxPuntaje = jugadores[0];
+		int jugadorMax = 1;
+
+		for(int i = 1; i< cantidadJugadores; i++){
+			if(jugadores[i]> maxPuntaje){
+				maxPuntaje = jugadores[i];
+				jugadorMax = i + 1 ;
+			}
+		}
+        
+		System.out.println("El jugador con el puntaje mas alto es: " + jugadorMax + " con "+ maxPuntaje + ".");
     }
 
 	
